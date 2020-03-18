d9825ac654ebad42fa9d74daadf1384e1aa9e46f
Sana Ebadi <sanaebadi97@gmail.com>
2020-03-18 07:14:13 -0400

    added utils class and sum method

---
+++ app/src/main/java/info/sanaebadi/androidtestingwithjunitandespresso/MyUtils.kt
@@ -0,0 +1,17 @@
+package info.sanaebadi.androidtestingwithjunitandespresso
+
+class MyUtils {
+
+    companion object {
+
+        fun sum(numbers: List<Int>): Int {
+
+            var summation = 0
+            for (sum: Int in numbers) {
+                summation += sum
+            }
+
+            return summation
+        }
+    }
+}
