113b5f9e0e8cb74eaca33cdd1e1917fd5b0b41ce
Sana Ebadi <sanaebadi97@gmail.com>
2020-03-19 08:51:54 -0400

    added MainActivity Test class in android test  with Junit4

---
+++ app/src/androidTest/java/info/sanaebadi/androidtestingwithjunitandespresso/MainActivityTest.kt
@@ -0,0 +1,21 @@
+package info.sanaebadi.androidtestingwithjunitandespresso
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    //we need the rule to launch the activity before running the test
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("info.sanaebadi.androidtestingwithjunitandespresso", appContext.packageName)
+    }
+}
